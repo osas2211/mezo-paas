4bde181f5c16af8466421023a549e5f24c227a9b
Osaretin Frank <osaretin.frank10@gmail.com>
2026-05-24 15:26:40 +0100

    fix: save error build logs

--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -396,6 +396,7 @@ export class ProjectService {
         data: {
           status: status,
           deploymentFinishedAt: new Date().toISOString(),
+          buildLogs: logs || '',
         },
       });
     }
